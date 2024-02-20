699726076c6a09115727013cf93b013f13e42528
Cassiano Castro <development.castro@gmail.com>
2024-02-20 16:20:37 -0300

    Added debug function calls

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
 require_once __DIR__ . '/lib/autoload.php';
 
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+
 /**
  *
  */
 function index(): void
 {
-    // code ...
+    (new api\services\EmployeesServices())->showEmployees();
 }
 
 index();
