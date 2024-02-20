ecf098deea4318d859e97b66bcece46282ce37e6
Cassiano Castro <development.castro@gmail.com>
2024-02-20 18:01:39 -0300

    Replaced app starter

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
 require_once __DIR__ . '/lib/autoload.php';
 
-error_reporting(E_ALL);
-ini_set("display_errors", true);
-
 /**
  *
  */
 function index(): void
 {
-    (new api\services\EmployeesServices())->showEmployees();
+    error_reporting(E_ALL);
+    ini_set("display_errors", true);
+
+    (new api\routes\Router())->dispatch();
 }
 
 index();
