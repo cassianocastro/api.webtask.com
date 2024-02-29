92e318320ff8e1f910b1940d7a958db3d52a9d07
Cassiano Castro <development.castro@gmail.com>
2024-02-29 18:25:07 -0300

    Changed implementation of the methods

--- api/services/ContractsServices.php
+++ api/services/ContractsServices.php
@@ -3,7 +3,8 @@ declare(strict_types=1);
 
 namespace api\services;
 
-use api\models\{ DBConfig, ConnectionFactory };
+use api\models\{ DBConfig, ConnectionFactory, InputReader};
+use api\models\entities\Contract;
 use api\models\repository\ContractsRepository;
 
 /**
@@ -14,20 +15,45 @@ final class ContractsServices
 
     public function addContract(): void
     {
-        $contract   = $_POST[""];
+        $array      = (new InputReader())->read();
 
         $config     = new DBConfig("localhost", "mysql", "webtask", "php", "php", 3306);
         $connection = (new ConnectionFactory())->create($config);
 
-        (new ContractsRepository($connection))->insert($contract);
+        try
+        {
+            (new ContractsRepository($connection))->insert(
+                new Contract(0, $array["register"], $array["office"], $array["admission"], $array["wage"])
+            );
+
+            echo 200;
+        }
+        catch ( \Exception $e )
+        {
+            echo 500;
+        }
     }
 
     public function updateContract(): void
     {
+        $array      = (new InputReader())->read();
+
+        $contract   = new Contract(0, $array["register"], $array["office"], $array["admission"], $array["wage"]);
+        $cpf        = 12345678900;
+
         $config     = new DBConfig("localhost", "mysql", "webtask", "php", "php", 3306);
         $connection = (new ConnectionFactory())->create($config);
 
-        // (new ContractsRepository($connection))->updateContract($contract);
+        try
+        {
+            (new ContractsRepository($connection))->update($contract, $cpf);
+
+            echo 200;
+        }
+        catch ( \Exception $e )
+        {
+            echo 500;
+        }
     }
 
     public function deleteContract(): void
@@ -43,6 +69,19 @@ final class ContractsServices
         $config     = new DBConfig("localhost", "mysql", "webtask", "php", "php", 3306);
         $connection = (new ConnectionFactory())->create($config);
 
-        // (new ContractsRepository($connection))->get();
+        $cpf = 12345678900;
+
+        try
+        {
+            $contract = (new ContractsRepository($connection))->getContractFromEmployee($cpf);
+
+            header("Content-Type: application/json;charset=utf-8", false, 200);
+
+            echo json_encode($contract);
+        }
+        catch ( \Exception $e )
+        {
+            echo $e->getMessage() . PHP_EOL, 500;
+        }
     }
 }
